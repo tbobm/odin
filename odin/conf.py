fd5af64a673f029d6eb698e0447d80b54f9adf58
Theo Massard <theo@memento-solution.com>
2020-03-21 19:15:35 +0100

    Fix SLEEPY_DURATION TypeError when overriding

--- odin/conf.py
+++ odin/conf.py
@@ -3,5 +3,5 @@ import os
 
 
 HTTP_TIMEOUT = os.environ.get('ODIN_HTTP_TIMEOUT', 5)
-SLEEPY_DURATION = os.environ.get('ODIN_SLEEP_DURATION', 30)
+SLEEPY_DURATION = int(os.environ.get('ODIN_SLEEP_DURATION', "30"))
 CONFIG_LOCATION = os.environ.get("ODIN_CONFIG_FILE", "/app/odin.yaml")
